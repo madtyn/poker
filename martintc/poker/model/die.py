1da19378867c52de5eb02474ce965f05d7ec85d0
madtyn <madtyn@vant>
2017-03-12 02:05:25 +0100

    PEP8. Uses NotAllowedError. Added numUses.

--- martintc/poker/model/die.py
+++ martintc/poker/model/die.py
@@ -1,5 +1,6 @@
 import random
 from martintc.poker.model.utils import Utils, compFunc
+from martintc.poker.model.errors import NotAllowedError
 
 '''
 Created on 18 feb. 2017
@@ -7,65 +8,94 @@ Created on 18 feb. 2017
 @author: madtyn
 '''
 
+
 class Die(object):
     '''
     This is the class which will simulate a die.
     It can hold a value. It can be hidden or not. Etc...
     '''
     # This makes the number to letter conversion
-    LETTERS = {1:'N', 2:'R', 3:'J', 4:'Q', 5:'K', 6:'A'}
-    
+    LETTERS = {1: 'N', 2: 'R', 3: 'J', 4: 'Q', 5: 'K', 6: 'A'}
+
     # The inverse dict, for reverse conversion (letter to number)
     NUMBERS = Utils.reverseDict(LETTERS)
-    
+
     # FACES = [1,2,3,4,5,6]
     FACES = sorted(LETTERS.keys())
 
-    def __init__(self, value=False, throw=True, hide=True):
+    def __init__(self, value=False, throw=True, hide=True, numUses=1):
         '''
         Constructor
-        
+
         :param throw: if True, it assigns a random value to this Die
         :param hide: if True, it hides this die and sets its hidden status to True
-        :param value: 
+        :param value:
         '''
+        self.numUses = numUses
+        self.hidden = hide
+
         if value or value is None:
             self.val = value
         elif throw:
             self.throw()
         else:
             self.val = None
-        self.hidden = hide
-        
+
     def throw(self):
         '''
         Generates a random number from the list [1,2,3,4,5,6]
         '''
-        self.val = random.choice(Die.FACES) 
-        return self.val
-    
+        if self.numUses:
+            self.val = random.choice(Die.FACES)
+            self.numUses -= 1
+            return self.val
+        raise NotAllowedError('El dado {} ha agotado sus tiradas'.format(self))
+
+    def lie(self):
+        '''
+        TODO
+        Devuelve un nuevo dado con igual o superior valor al ya existente
+        '''
+        return Die(value=random.choice([x for x in Die.FACES if x >= self.val]), hide=True, numUses=0)
+
+    def restore(self, numUses=1):
+        '''
+        It restores the number of uses of the die
+        :param numUses: the new numeric value for the number of uses allowed
+        '''
+        self.numUses = numUses
+
     def getLetter(self):
         '''
         Converts the numeric value received as parameter to the letter in the die
         '''
         return Die.LETTERS.get(self.val, 'E')
-    
+
     def hide(self):
         '''
         Hides the die's value from the players
         '''
         self.hidden = True
-    
+
     def show(self):
         '''
         Shows the die's value to the players
         '''
         self.hidden = False
-    
+
+    def reset(self):
+        '''
+        Resets this die to None
+        '''
+        self = None
+
     def test(self):
+        '''
+        Tests this die
+        '''
         if self.val and self.val not in Die.FACES:
-            raise ValueError('Die value out of bounds: {}'.fomat(self.val))
-        
+            raise ValueError('Die value out of bounds: {}'.format(self.val))
+
     # SORTING FUNCTIONS
     @compFunc
     def __eq__(self, other):
@@ -74,7 +104,7 @@ class Die(object):
         :param other: the other die to compare with
         '''
         return self.val == other.val
-    
+
     @compFunc
     def __ne__(self, other):
         '''
@@ -82,7 +112,7 @@ class Die(object):
         :param other: the other die to compare with
         '''
         return not self.__eq__(other)
-    
+
     @compFunc
     def __ge__(self, other):
         '''
@@ -90,7 +120,7 @@ class Die(object):
         :param other: the other die to compare with
         '''
         return self.val >= other.val
-    
+
     @compFunc
     def __gt__(self, other):
         '''
@@ -98,7 +128,7 @@ class Die(object):
         :param other: the other die to compare with
         '''
         return self.val > other.val
-    
+
     @compFunc
     def __le__(self, other):
         '''
@@ -117,17 +147,17 @@ class Die(object):
 
     def __repr__(self, *args, **kwargs):
         '''
-        Unambiguous string representation for this object, 
+        Unambiguous string representation for this object,
         useful for logging or debugging purposes
         '''
         return '{0!s}{1}'.format(self.val, '?' if self.hidden else '')
-    
+
     def __str__(self, *args, **kwargs):
         '''
-        Nice representation for this object, 
+        Nice representation for this object,
         useful for showing in the app
         '''
-        return (self.getLetter() if self.val else '_')+('?' if self.hidden else '')
+        return (self.getLetter() if self.val else '_') + ('?' if self.hidden else '')
 
 if __name__ == "__main__":
     print(Die())
