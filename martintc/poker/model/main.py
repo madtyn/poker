70f06998fd63a39f80bcbfd27ef8573c1857475f
madtyn <madtyn@vant>
2017-03-13 01:27:49 +0100

    Actualizacion del esqueleto del main() de prueba

--- martintc/poker/model/main.py
+++ martintc/poker/model/main.py
@@ -3,18 +3,29 @@ Created on 26 feb. 2017
 
 @author: madtyn
 '''
-from martintc.poker.model.diceset import DiceSet
 import random
 
-if __name__ == '__main__':
+from martintc.poker.model.diceset import DiceSet
+
+
+def main():
+    '''
+    Main function
+    '''
     numPlayers = 3
     print('Liar poker game')
     print('Initializing players')
     players = ['p{}'.format(x + 1) for x in range(numPlayers)]
     currentPlayer = 0
     dice = DiceSet()
-    print('Player {} has thrown {!s}'.format(players[currentPlayer], dice))
+    print('Player {} has thrown {!r}'.format(players[currentPlayer], dice))
+
     print('Player {} hides/shows some dice'.format(players[currentPlayer]))
-    print('Player {} says {!s}'.format(players[currentPlayer], dice.hand()))
     _ = [die.show() for die in dice if random.choice([True, False])]
-    print('{!r}'.format(dice))
+
+    print('Players can see now {0!s} of {0!r}'.format(dice))
+    print('Player does not give up {} says {!s}'.format(players[currentPlayer], dice.hand()))
+    print()
+
+if __name__ == '__main__':
+    main()
