cac9b436ab5846f80e383a6d159b87ca32233c51
madtyn <madtyn@gmail.com>
2017-10-10 02:35:42 +0200

    Docs

--- martintc/poker/model/diceset.py
+++ martintc/poker/model/diceset.py
@@ -15,21 +15,21 @@ from martintc.poker.model.utils import compFunc
 
 
 class DiceSet(object):
-    '''
+    """
     This class represents a specific move from one player
     gathering the values from different dice to sum up an only value
     for the whole move.
-    '''
+    """
 
-    def __init__(self, inputList=None, throw=True, length=5):
-        '''
+    def __init__(self, input_list=None, throw=True, length=5):
+        """
         Constructor
-        :param inputList: if provided, the list of numeric values for this diceset
+        :param input_list: if provided, the list of numeric values for this diceset
         :param throw: if True generates a random diceset by throwing all its dice
         :param length: the length of the diceset, 5 by default
-        '''
-        if inputList:
-            self.dice = [elem if isinstance(elem, Die) else Die(elem) for elem in inputList[:length]]
+        """
+        if input_list:
+            self.dice = [elem if isinstance(elem, Die) else Die(elem) for elem in input_list]
         elif throw:
             self.dice = [Die(throw=throw) for _ in range(length)]
         else:
@@ -38,37 +38,40 @@ class DiceSet(object):
 
     def lie(self, fear=0.75, needToLie=True):
         #TODO list
-        '''
+        """
         -Devuelve una nueva jugada estrictamente superior, pero el objetivo es que mienta con una probabilidad creible
         y que sea ajustable segun la dificultad
         Hay que tener en cuenta que puede cambiar según los dados tirados, solo habría que mentir sobre los dados tirados Y ocultos
         Tambien hay que tener en cuenta que la posibilidad de superar podria ser unica
-        '''
+        """
         # probs = float(self.surpassProb())  # Estima la dificultad de superar
         # distance = None  # TODO
         # Comparar la diferencia entre self.dice y la maxima puntuacion que se puede sacar tirando dados ocultos
 
         # Iniciamos el dado en el minimo posible para superarlo
         dice = DiceSet([Die(min(Die.FACES)) if d.hidden else deepcopy(d) for d in self.dice])
+
+        # TODO - It might be the case when the dice can't be surpassed only throwing the hidden ones
+        # TODO      Example [2?, 2, 2, 3, 6]
         while dice <= self:  # or (probs - dice.surpassProb()) > fear:  # TODO
             dice = DiceSet([d.lie() if d.hidden else deepcopy(d) for d in self.dice])
             # Tipificar la jugada/mentira entre 0 y 1 para obtener un coeficiente de "credibilidad" (+ diferencia hacia arriba, + dificil)
             # Si es necesaria, mas dificil aun
         return dice
 
-    def surpassProb(self, numDice=None):
-        '''
+    def surpassProb(self, num_dice=None):
+        """
         Calculates the exact probability for surpassing this diceset's value
-        with the exact number of 'numDice' dice.
+        with the exact number of 'num_dice' dice.
 
-        Example: If numDice = 3, this explores the probability to surpass this diceset
+        Example: If num_dice = 3, this explores the probability to surpass this diceset
         with all the 3-dice combinations
-        '''
-        favorableCases = 0
+        """
+        favorable_cases = 0
         cases = 0
         '''
-        We obtain as many times FACES(=[NRJQKA]) as number of dice (=numDice) in this diceset.
-        For numDice=5 dice, it would be 5 lists [NRJQKA], [NRJQKA],...x2..., [NRJQKA]
+        We obtain as many times FACES(=[NRJQKA]) as number of dice (=num_dice) in this diceset.
+        For num_dice=5 dice, it would be 5 lists [NRJQKA], [NRJQKA],...x2..., [NRJQKA]
 
         Then we do a cartesian product
         (with FACESxFACES it would get all pairs (N,N) (N,R)..(N,A), (R,N),..,(A,A) )
@@ -77,98 +80,98 @@ class DiceSet(object):
         So we have now all cases ready for being converted to list and used
 
         '''
-        if not numDice:
-            numDice = len(self.dice)
-        n_FACES = repeat(Die.FACES, numDice)
-        allCases = [DiceSet(list(elem)) for elem in product(*n_FACES)]
-        for case in allCases:
+        if not num_dice:
+            num_dice = len(self.dice)
+        n_FACES = repeat(Die.FACES, num_dice)
+        all_cases = [DiceSet(list(elem)) for elem in product(*n_FACES)]
+        for case in all_cases:
             cases += 1
             if case > self:
-                favorableCases += 1
+                favorable_cases += 1
 
-        return Fraction(favorableCases, cases)
+        return Fraction(favorable_cases, cases)
 
     def hand(self):
-        '''
+        """
         Returns the poker hand from these dice
-        '''
-        numberEqualFaces = sorted(list(self.frequencies().values()), reverse=True)
-        return Hand.getHand(numberEqualFaces)
+        """
+        number_equal_faces = sorted(list(self.frequencies().values()), reverse=True)
+        return Hand.getHand(number_equal_faces)
 
     def frequencies(self, letter=False):
-        '''
+        """
         Gets a dict counting the faces and the number of faces in these dice.
 
         :param letter: if True, it returns the letters/faces, if not numbers are returned
-        '''
+        """
         return Counter([d.getLetter() if letter else d.val for d in self.dice])
 
-    def availableDice(self):
+    def available_dice(self):
         return DiceSet([deepcopy(d) for d in self.dice if d.numUses > 0])
 
-    def notAvailableDice(self):
+    def not_available_dice(self):
         return DiceSet([deepcopy(d) for d in self.dice if d.numUses == 0])
 
-    def visibleDice(self):
+    def visible_dice(self):
         return DiceSet([deepcopy(d) for d in self.dice if not d.hidden])
 
-    def hiddenDice(self):
-        return DiceSet([deepcopy(d) for d in self.dice if not d.hidden])
+    def hidden_dice(self):
+        return DiceSet([deepcopy(d) for d in self.dice if d.hidden])
 
     def sort(self):
-        '''
+        """
         Sorts the dice from left to right, minor values to max ones
-        '''
+        """
         self.dice.sort()
 
     def show(self):
-        '''
+        """
         Shows the dice
-        '''
+        """
         for die in self.dice:
             die.show()
 
     def hide(self):
-        '''
+        """
         Hides the dice
-        '''
+        """
         for die in self.dice:
             die.hide()
 
     def restore(self):
-        '''
+        """
         Restores the numbers of uses of these dice
-        '''
+        """
         for die in self.dice:
             die.restore()
 
     def reset(self):
-        '''
+        """
         Resets the dice to None
-        '''
+        """
         for die in self.dice:
             die.reset()
 
     def test(self):
-        '''
+        """
         Tests the dice
-        '''
+        """
         for die in self.dice:
             die.test()
 
     # PRIVATE API FUNCTIONS
 
     def __iter__(self):
-        '''
+        """
         Returns itself as an iterator
-        '''
+        """
         self.index = -1
         return self
 
     def __next__(self):
-        '''
+        """
         Returns next die in the iteration or stops
-        '''
+        """
         self.index += 1
         if self.index >= len(self.dice):
             self.index = -1
@@ -177,34 +180,34 @@ class DiceSet(object):
 
     # OPERATOR FUNCTIONS
     def __contains__(self, dieObj):
-        '''
+        """
         Check if dieObj is contained in this diceset. Used by 'in' operator
 
         :param dieObj: the die to check
-        '''
+        """
         if isinstance(dieObj, Die):
             return dieObj in self.dice
 
     def __add__(self, other):
-        '''
+        """
         Returns a new diceset as the addition between dicesets, which would be:
 
         [1,1] + [3,4] == [1,1,3,4]
         [N,N] + [J,Q] == [N,N,J,Q]
 
         :param other: the other diceset
-        '''
+        """
         dset = DiceSet(deepcopy(self.dice) + deepcopy(other.dice))
         return dset
 
     def __sub__(self, other):
-        '''
+        """
         Returns a new diceset with these dice and with the dice in other removed
 
         [1,1,3,3,4] - [1,3,4] == [1,3]
 
         :param other: the other diceset
-        '''
+        """
         _dice = deepcopy(self.dice)
         _other = deepcopy(other)
 
@@ -218,25 +221,25 @@ class DiceSet(object):
             return dset
 
     def __truediv__(self, other):
-        '''
+        """
         Returns a new diceset with these dice and all the dice in other removed
 
         [1,1,3,3,4] / [1,3,4] == []
 
         :param other: the other diceset
-        '''
+        """
         dset = DiceSet([deepcopy(d) for d in self.dice if d not in other.dice])
         return dset
 
     def __mul__(self, other):
-        '''
+        """
         Returns a new diceset
 
         [1,2] * 3  == [1,2,1,2,1,2]
                        1   2   3
 
         :param other: an integer
-        '''
+        """
         if isinstance(other, int):
             dset = DiceSet(deepcopy(self.dice) * other)
             return dset
@@ -246,10 +249,10 @@ class DiceSet(object):
     # SORTING FUNCTIONS
     @compFunc
     def __cmp__(self, other):
-        '''
+        """
         Compares two diceset looking at its dice's faces/points
         :param other: the other object to compare with
-        '''
+        """
         li1 = list(self.frequencies().items())
         li1 = sorted(li1, key=itemgetter(1, 0), reverse=True)
 
@@ -268,26 +271,26 @@ class DiceSet(object):
 
     @compFunc
     def __eq__(self, other):
-        '''
+        """
         When using the operator (==), compares if this diceset is equal to another one
         :param other: the other diceset to compare with
-        '''
+        """
         return self.hand() == other.hand() and self.__cmp__(other) == 0
 
     @compFunc
     def __ne__(self, other):
-        '''
+        """
         When using the operator (!=), compares if this diceset is different to another one
         :param other: the other diceset to compare with
-        '''
+        """
         return not self.__eq__(other)
 
     @compFunc
     def __ge__(self, other):
-        '''
+        """
         When using the operator (>=), compares if this diceset is greater or equal than another one
         :param other: the other diceset to compare with
-        '''
+        """
         if self.hand() == other.hand():
             return self.__cmp__(other) >= 0
         else:
@@ -295,10 +298,10 @@ class DiceSet(object):
 
     @compFunc
     def __gt__(self, other):
-        '''
+        """
         When using the operator (>), compares if this diceset is greater than another one
         :param other: the other diceset to compare with
-        '''
+        """
         if self.hand() == other.hand():
             return self.__cmp__(other) > 0
         else:
@@ -306,10 +309,10 @@ class DiceSet(object):
 
     @compFunc
     def __le__(self, other):
-        '''
+        """
         When using the operator (<=), compares if this diceset is less than another one
         :param other: the other diceset to compare with
-        '''
+        """
         if self.hand() == other.hand():
             return self.__cmp__(other) <= 0
         else:
@@ -317,37 +320,38 @@ class DiceSet(object):
 
     @compFunc
     def __lt__(self, other):
-        '''
+        """
         When using the operator (<), compares if this diceset is less than another one
         :param other: the other diceset to compare with
-        '''
+        """
         if self.hand() == other.hand():
             return self.__cmp__(other) < 0
         else:
             return self.hand() < other.hand()
 
     def __len__(self):
-        '''
+        """
         Returns the length of dice in this combination
-        '''
+        """
         return len(self.dice)
 
     def __repr__(self, *args, **kwargs):
-        '''
+        """
         Unambiguous string representation for this object,
         useful for logging or debugging purposes
-        '''
+        """
         freqs = {}
         for k, v in self.frequencies(letter=True).items():
             freqs[k] = v
         return '[{}] {}'.format(','.join([repr(die) for die in self.dice]), freqs)
 
     def __str__(self, *args, **kwargs):
-        '''
+        """
         Nice readable representation for this object, useful for
         showing in the app
-        '''
+        """
         return '{} [{}]'.format(self.hand().name, ','.join([str(die) for die in self.dice]))
+
 
 if __name__ == "__main__":
     # Constructor testing
